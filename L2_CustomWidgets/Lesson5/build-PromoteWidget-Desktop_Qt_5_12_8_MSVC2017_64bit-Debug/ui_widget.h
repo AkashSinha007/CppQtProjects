882e0a3f71e21ac7a7666700aa63cb17db68bc1a
AkashSinha007 <akashkumarsinha007@gmail.com>
2021-03-25 22:42:46 +0530

    Adding WaterLevelMonitoringSystem

---
+++ L2_CustomWidgets/Lesson5/build-PromoteWidget-Desktop_Qt_5_12_8_MSVC2017_64bit-Debug/ui_widget.h
@@ -0,0 +1,70 @@
+/********************************************************************************
+** Form generated from reading UI file 'widget.ui'
+**
+** Created by: Qt User Interface Compiler version 5.12.8
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_WIDGET_H
+#define UI_WIDGET_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QWidget>
+#include "indicator.h"
+#include "watertank.h"
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Widget
+{
+public:
+    QHBoxLayout *horizontalLayout_2;
+    QHBoxLayout *horizontalLayout;
+    WaterTank *waterTank;
+    Indicator *indicator;
+
+    void setupUi(QWidget *Widget)
+    {
+        if (Widget->objectName().isEmpty())
+            Widget->setObjectName(QString::fromUtf8("Widget"));
+        Widget->resize(520, 365);
+        horizontalLayout_2 = new QHBoxLayout(Widget);
+        horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        waterTank = new WaterTank(Widget);
+        waterTank->setObjectName(QString::fromUtf8("waterTank"));
+
+        horizontalLayout->addWidget(waterTank);
+
+        indicator = new Indicator(Widget);
+        indicator->setObjectName(QString::fromUtf8("indicator"));
+
+        horizontalLayout->addWidget(indicator);
+
+
+        horizontalLayout_2->addLayout(horizontalLayout);
+
+
+        retranslateUi(Widget);
+
+        QMetaObject::connectSlotsByName(Widget);
+    } // setupUi
+
+    void retranslateUi(QWidget *Widget)
+    {
+        Widget->setWindowTitle(QApplication::translate("Widget", "Widget", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Widget: public Ui_Widget {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_WIDGET_H
